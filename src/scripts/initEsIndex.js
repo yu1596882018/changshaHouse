9203c4e3a590b65c8a445522cb0008e2f6729048
xinyu.wang <xinyu.wang@hexasino.com>
2022-07-11 17:23:28 +0800

    feat: 开启es上报

--- src/scripts/initEsIndex.js
+++ src/scripts/initEsIndex.js
@@ -1,4 +1,4 @@
-const esClient = require('./es')
+const esClient = require('./../db/es')
 esClient.create({
   "index": "server_res_logs",
   "type": "_doc",
@@ -98,5 +98,53 @@ esClient.create({
     "markUser": "NWXnMEMTpT1657521681788",
     "markUv": "bYNNDMJpmE1657521681789",
     "pageId": "changshaHouseMoblie"
+  }
+})
+esClient.create({
+  "index": "web_err_logs",
+  "type": "_doc",
+  "id": "d48c4230-00f6-11ed-9150-99b16d9b8af4",
+  "pipeline": "geoip",
+  "body": {
+    "category": "js_error",
+    "logType": "Warning",
+    "deviceType": "PC",
+    "OS": "Windows",
+    "OSVersion": "10.0",
+    "screenHeight": 864.11,
+    "screenWidth": 1536.11,
+    "language": "zh_CN",
+    "netWork": "4g",
+    "orientation": "横屏",
+    "browserInfo": "Chrome（版本: 103.0.0.0&nbsp;&nbsp;内核: Blink）",
+    "fingerprint": "28507245",
+    "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+    "browser": "Chrome",
+    "browserVersion": "103.0.0",
+    "timestamp": "2022-07-11T08:52:19.003Z",
+    "ip": "202.104.136.67",
+    "errorInfo": "错误类别: js_error\r\n日志信息: Uncaught ReferenceError: aaa is not defined\r\nurl: \r\n错误行号: 1\r\n错误列号: 1\r\n错误栈: ReferenceError: aaa is not defined\n    at <anonymous>:1:1\r\n"
+  }
+})
+esClient.create({
+  "index": "server_err_logs",
+  "type": "_doc",
+  "id": "773668d0-00f7-11ed-bc15-6dc39f8c0e75",
+  "pipeline": "geoip",
+  "body": {
+    "requestMethod": "OPTIONS",
+    "requestHost": "localhost:8899",
+    "requestOriginalUrl": "/houseInfoList?offset=0&limit=20",
+    "requestMatchedRoute": "null",
+    "ip": "202.104.136.67",
+    "requestTime": 4.11,
+    "timestamp": "2022-07-11T08:57:09.368Z",
+    "browser": "Chrome",
+    "browserVersion": "103.0.0",
+    "system": "Windows",
+    "systemVersion": "10.0.0",
+    "errName": "ReferenceError",
+    "errMessage": "ssss is not defined",
+    "errStack": "ReferenceError: ssss is not defined\n    at D:\\workspace\\changshaHouse\\src\\app.js:20:1\n    at processTicksAndRejections (internal/process/task_queues.js:93:5)"
   }
 })
